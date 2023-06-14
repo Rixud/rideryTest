a779f1c2bc464c725c24cfa34bc3803c53117296
Luis Guerra Di Martino <lmartino@ubiipagos.com>
2023-06-14 10:31:11 +0200

    Initial Commit

---
+++ RideryTest/RideryTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RideryTestApp.swift
+//  RideryTest
+//
+//  Created by ubiipagos on 14/6/23.
+//
+
+import SwiftUI
+
+@main
+struct RideryTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
